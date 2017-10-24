5c5b58337a6a372dbc726c15a546eaa902166b0a
Doug Shook <dshook@wustl.edu>
2017-10-24 12:51:25 -0500

    hw3 starter code

---
+++ CSE530-hw1/src/hw3/BPlusTree.java
@@ -0,0 +1,33 @@
+package hw3;
+
+
+import hw1.Field;
+
+public class BPlusTree {
+    
+    public BPlusTree(int degree) {
+    	//your code here
+    }
+    
+    public LeafNode search(Field f) {
+    	//your code here
+    	return null;
+    }
+    
+    public void insert(Entry e) {
+    	//your code here
+    }
+    
+    public void delete(Entry e) {
+    	//your code here
+    }
+    
+    public Node getRoot() {
+    	//your code here
+    	return null;
+    }
+    
+
+
+	
+}
---
+++ CSE530-hw1/src/hw3/InnerNode.java
@@ -0,0 +1,32 @@
+package hw3;
+
+import java.util.ArrayList;
+
+import hw1.Field;
+
+public class InnerNode implements Node {
+	
+	public InnerNode(int degree) {
+		//your code here
+	}
+	
+	public ArrayList<Field> getKeys() {
+		//your code here
+		return null;
+	}
+	
+	public ArrayList<Node> getChildren() {
+		//your code here
+		return null;
+	}
+
+	public int getDegree() {
+		//your code here
+		return 0;
+	}
+	
+	public boolean isLeafNode() {
+		return false;
+	}
+
+}
