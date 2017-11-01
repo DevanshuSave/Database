aa9f3938361adc980630b015fdc9ca328dad7db8
devan <devan@DESKTOP-MFRN38J.dhcp.wustl.edu>
2017-11-01 15:24:11 -0500

    # WARNING: head commit changed in the meantime
    start of hw3

--- CSE530-hw1/src/hw3/InnerNode.java
+++ CSE530-hw1/src/hw3/InnerNode.java
@@ -6,23 +6,31 @@ import hw1.Field;
 
 public class InnerNode implements Node {
 	
+	//Create variables
+	int degree;
+	ArrayList<Node> children;
+	ArrayList<Field> keys;
+	
 	public InnerNode(int degree) {
 		//your code here
+		this.degree = degree;
+		this.children = new ArrayList<Node>(degree+1);
+		this.keys = new ArrayList<Field>(degree);
 	}
 	
 	public ArrayList<Field> getKeys() {
 		//your code here
-		return null;
+		return this.keys;
 	}
 	
 	public ArrayList<Node> getChildren() {
 		//your code here
-		return null;
+		return this.children;
 	}
 
 	public int getDegree() {
 		//your code here
-		return 0;
+		return this.degree;
 	}
 	
 	public boolean isLeafNode() {
--- CSE530-hw1/src/hw3/LeafNode.java
+++ CSE530-hw1/src/hw3/LeafNode.java
@@ -4,18 +4,23 @@ import java.util.ArrayList;
 
 public class LeafNode implements Node {
 	
+	int degree;
+	ArrayList<Entry> entries;
+	
 	public LeafNode(int degree) {
 		//your code here
+		this.degree=degree;
+		this.entries = new ArrayList<Entry>(degree);
 	}
 	
 	public ArrayList<Entry> getEntries() {
 		//your code here
-		return null;
+		return this.entries;
 	}
 
 	public int getDegree() {
 		//your code here
-		return 0;
+		return this.degree;
 	}
 	
 	public boolean isLeafNode() {
