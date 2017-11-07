315ff92bae6a60572e3ea63508437a856203885a
Doug Shook <dshook@wustl.edu>
2017-11-07 12:44:27 -0600

    assignment 4

---
+++ CSE530-hw1/src/hw1/BufferPool.java
@@ -0,0 +1,130 @@
+package hw1;
+
+import java.io.*;
+
+/**
+ * BufferPool manages the reading and writing of pages into memory from
+ * disk. Access methods call into it to retrieve pages, and it fetches
+ * pages from the appropriate location.
+ * <p>
+ * The BufferPool is also responsible for locking;  when a transaction fetches
+ * a page, BufferPool which check that the transaction has the appropriate
+ * locks to read/write the page.
+ */
+public class BufferPool {
+    /** Bytes per page, including header. */
+    public static final int PAGE_SIZE = 4096;
+
+    /** Default number of pages passed to the constructor. This is used by
+    other classes. BufferPool should use the numPages argument to the
+    constructor instead. */
+    public static final int DEFAULT_PAGES = 50;
+
+    /**
+     * Creates a BufferPool that caches up to numPages pages.
+     *
+     * @param numPages maximum number of pages in this buffer pool.
+     */
+    public BufferPool(int numPages) {
+        // your code here
+    }
+
+    /**
+     * Retrieve the specified page with the associated permissions.
+     * Will acquire a lock and may block if that lock is held by another
+     * transaction.
+     * <p>
+     * The retrieved page should be looked up in the buffer pool.  If it
+     * is present, it should be returned.  If it is not present, it should
+     * be added to the buffer pool and returned.  If there is insufficient
+     * space in the buffer pool, an page should be evicted and the new page
+     * should be added in its place.
+     *
+     * @param tid the ID of the transaction requesting the page
+     * @param tableId the ID of the table with the requested page
+     * @param pid the ID of the requested page
+     * @param perm the requested permissions on the page
+     */
+    public HeapPage getPage(int tid, int tableId, int pid, Permissions perm)
+        throws Exception {
+        // your code here
+        return null;
+    }
+
+    /**
+     * Releases the lock on a page.
+     * Calling this is very risky, and may result in wrong behavior. Think hard
+     * about who needs to call this and why, and why they can run the risk of
+     * calling it.
+     *
+     * @param tid the ID of the transaction requesting the unlock
+     * @param tableID the ID of the table containing the page to unlock
+     * @param pid the ID of the page to unlock
+     */
+    public  void releasePage(int tid, int tableId, int pid) {
+        // your code here
+    }
+
+    /** Return true if the specified transaction has a lock on the specified page */
+    public   boolean holdsLock(int tid, int tableId, int pid) {
+        // your code here
+        return false;
+    }
+
+    /**
+     * Commit or abort a given transaction; release all locks associated to
+     * the transaction. If the transaction wishes to commit, write
+     *
+     * @param tid the ID of the transaction requesting the unlock
+     * @param commit a flag indicating whether we should commit or abort
+     */
+    public   void transactionComplete(int tid, boolean commit)
+        throws IOException {
+        // your code here
+    }
+
+    /**
+     * Add a tuple to the specified table behalf of transaction tid.  Will
+     * acquire a write lock on the page the tuple is added to. May block if the lock cannot 
+     * be acquired.
+     * 
+     * Marks any pages that were dirtied by the operation as dirty
+     *
+     * @param tid the transaction adding the tuple
+     * @param tableId the table to add the tuple to
+     * @param t the tuple to add
+     */
+    public  void insertTuple(int tid, int tableId, Tuple t)
+        throws Exception {
+        // your code here
+    }
+
+    /**
+     * Remove the specified tuple from the buffer pool.
+     * Will acquire a write lock on the page the tuple is removed from. May block if
+     * the lock cannot be acquired.
+     *
+     * Marks any pages that were dirtied by the operation as dirty.
+     *
+     * @param tid the transaction adding the tuple.
+     * @param tableId the ID of the table that contains the tuple to be deleted
+     * @param t the tuple to add
+     */
+    public  void deleteTuple(int tid, int tableId, Tuple t)
+        throws Exception {
+        // your code here
+    }
+
+    private synchronized  void flushPage(int tableId, int pid) throws IOException {
+        // your code here
+    }
+
+    /**
+     * Discards a page from the buffer pool.
+     * Flushes the page to disk to ensure dirty pages are updated on disk.
+     */
+    private synchronized  void evictPage() throws Exception {
+        // your code here
+    }
+
+}
---
+++ CSE530-hw1/src/test/TransactionTest.java
@@ -0,0 +1,109 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import hw1.BufferPool;
+import hw1.Catalog;
+import hw1.Database;
+import hw1.HeapFile;
+import hw1.HeapPage;
+import hw1.Permissions;
+import hw1.Tuple;
+import hw1.TupleDesc;
+
+public class TransactionTest {
+	
+	private Catalog c;
+	private BufferPool bp;
+	private HeapFile hf;
+	private TupleDesc td;
+	private int tid;
+	
+	@Before
+	public void setup() {
+		
+		try {
+			Files.copy(new File("testfiles/test.dat.bak").toPath(), new File("testfiles/test.dat").toPath(), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			System.out.println("unable to copy files");
+			e.printStackTrace();
+		}
+		
+		c = Database.getCatalog();
+		c.loadSchema("testfiles/test.txt");
+		
+		int tableId = c.getTableId("test");
+		td = c.getTupleDesc(tableId);
+		hf = c.getDbFile(tableId);
+		
+		bp = Database.getBufferPool();
+		
+		tid = c.getTableId("test");
+	}
+	@Test
+	public void testReleaseLocks() throws Exception {
+		bp.getPage(0, tid, 0, Permissions.READ_ONLY);
+	    bp.getPage(0, tid, 0, Permissions.READ_WRITE);
+	    bp.transactionComplete(0, true);
+
+	    bp.getPage(1, tid, 0, Permissions.READ_WRITE);
+	    bp.getPage(1, tid, 0, Permissions.READ_WRITE);
+	    assertTrue(true); //will only reach this point if locks are properly released
+	}
+	
+	public void testCommit() throws Exception {
+		Tuple t = new Tuple(td);
+		t.setField(0, new byte[] {0, 0, 0, (byte)131});
+		byte[] s = new byte[129];
+		s[0] = 2;
+		s[1] = 98;
+		s[2] = 121;
+		t.setField(1, s);
+		
+		bp.getPage(0, tid, 0, Permissions.READ_WRITE); //acquire lock for the page
+		bp.insertTuple(0, tid, t); //insert the tuple into the page
+		bp.transactionComplete(0, true); //should flush the modified page
+		
+		//reset the buffer pool, get the page again, make sure data is there
+		Database.resetBufferPool(BufferPool.DEFAULT_PAGES);
+		HeapPage hp = bp.getPage(1, tid, 0, Permissions.READ_ONLY);
+		Iterator<Tuple> it = hp.iterator();
+		assertTrue(it.hasNext());
+		it.next();
+		assertTrue(it.hasNext());
+		it.next();
+		assertFalse(it.hasNext());
+	}
+	
+	public void testAbort() throws Exception {
+		Tuple t = new Tuple(td);
+		t.setField(0, new byte[] {0, 0, 0, (byte)131});
+		byte[] s = new byte[129];
+		s[0] = 2;
+		s[1] = 98;
+		s[2] = 121;
+		t.setField(1, s);
+		
+		bp.getPage(0, tid, 0, Permissions.READ_WRITE); //acquire lock for the page
+		bp.insertTuple(0, tid, t); //insert the tuple into the page
+		bp.transactionComplete(0, false); //should abort, discard changes
+		
+		//reset the buffer pool, get the page again, make sure data is there
+		Database.resetBufferPool(BufferPool.DEFAULT_PAGES);
+		HeapPage hp = bp.getPage(1, tid, 0, Permissions.READ_ONLY);
+		Iterator<Tuple> it = hp.iterator();
+		assertTrue(it.hasNext());
+		it.next();
+		assertFalse(it.hasNext());
+	}
+
+}
