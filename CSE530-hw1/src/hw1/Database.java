234bbfddde42824847de153e8e288f09ae6a51a5
devan <devan@DESKTOP-MFRN38J>
2017-11-15 19:15:21 -0600

    manually resolved conflict

--- CSE530-hw1/src/hw1/Database.java
+++ CSE530-hw1/src/hw1/Database.java
@@ -1,29 +1,54 @@
-//Devanshu Save
 package hw1;
+
+import java.io.*;
+
+import hw1.BufferPool;
+
+/*
+ * Student 1 name:
+ * Student 2 name:
+ * Date: 
+ */
 
 /** Database is a class that initializes a static
     variable used by the database system (the catalog)
-    
+
     Provides a set of methods that can be used to access these variables
     from anywhere.
-*/
+ */
 
 public class Database {
 	private static Database _instance = new Database();
-    private final Catalog _catalog;
+	private final Catalog _catalog;
+	private BufferPool _bufferpool; 
 
-    private Database() {
-    	_catalog = new Catalog();
-    }
 
-    /** Return the catalog of the static Database instance*/
-    public static Catalog getCatalog() {
-        return _instance._catalog;
-    }
+	private Database() {
+		_catalog = new Catalog();
+		_bufferpool = new BufferPool(BufferPool.DEFAULT_PAGES);
+	}
 
-    //reset the database, used for unit tests only.
-    public static void reset() {
-    	_instance = new Database();
-    }
+	/** Return the catalog of the static Database instance*/
+	public static Catalog getCatalog() {
+		return _instance._catalog;
+	}
+
+	/** Return the buffer pool of the static Database instance*/
+	public static BufferPool getBufferPool() {
+		return _instance._bufferpool;
+	}
+
+	/** Method used for testing -- create a new instance of the
+    buffer pool and return it
+	 */
+	public static BufferPool resetBufferPool(int pages) {
+		_instance._bufferpool = new BufferPool(pages);
+		return _instance._bufferpool;
+	}
+
+	//reset the database, used for unit tests only.
+	public static void reset() {
+		_instance = new Database();
+	}
 
 }
